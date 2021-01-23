67211f8d0edbaeaf34d4b395da21e47cee824cde
Dariusz Gorczyca <dgorczyca@student.agh.edu.pl>
2021-01-23 15:48:32 +0100

    Import from txt

--- src/main/java/pl/edu/agh/hangman/ImportFromTxt.java
+++ src/main/java/pl/edu/agh/hangman/ImportFromTxt.java
@@ -1,5 +1,40 @@
 package pl.edu.agh.hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ImportFromTxt {
+	
+	ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	File txt = new File(classLoader.getResource("slowa.txt").getFile());
+	
+	public ArrayList<String> getWordsList () throws FileNotFoundException {
+		
+		Scanner scanner = new Scanner(txt);
+		ArrayList<String> words = new ArrayList<String>();
+	
+		while(scanner.nextLine() != null) {
+		
+		words.add(scanner.nextLine());
+		
+		}
+		
+		return words;
+		
+	}
+
+
+	public static void main(String[] args) throws FileNotFoundException  {
+
+
+		ImportFromTxt abc = new ImportFromTxt();
+		ArrayList<String> lala =  abc.getWordsList();
+		System.out.println(lala.toString());
+	
+     				
+    }
 
 }
